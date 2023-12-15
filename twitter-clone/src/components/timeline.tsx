5be1d8bccf1ff4802ca7833fcfd2fc3cc5c9b536
Naeun-Kim <naeun@Naeunui-MacBookPro.local>
2023-12-15 11:50:32 +0900

    change tweet query on real time

--- twitter-clone/src/components/timeline.tsx
+++ twitter-clone/src/components/timeline.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import Tweet from './tweets';
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore';
 import { db } from '../firebase';
+import { Unsubscribe } from 'firebase/auth';
 
 export interface ITweet {
   id: string;
@@ -17,29 +24,36 @@ const Wrapper = styled.div``;
 
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, 'tweets'),
-      orderBy('createdAt', 'desc')
-    );
-    const snapshot = await getDocs(tweetsQuery);
-    // snapshot.docs.forEach((doc) => console.log(doc.data()));
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, userId, username, createdAt, photo } = doc.data(); //extract data
-      //to be object
-      return {
-        tweet,
-        userId,
-        username,
-        createdAt,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, 'tweets'),
+        orderBy('createdAt', 'desc'),
+        limit(25)
+      );
+      // const snapshot = await getDocs(tweetsQuery);
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, userId, username, createdAt, photo } = doc.data(); //extract data
+          //to be object
+          return {
+            tweet,
+            userId,
+            username,
+            createdAt,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
     fetchTweets();
+    // https://react.dev/learn/synchronizing-with-effects#subscribing-to-events
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>

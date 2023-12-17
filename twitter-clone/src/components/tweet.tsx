f5277928cea90e956c24ea306ceac28f7266e0a4
Naeun-Kim <naeun@Naeunui-MacBookPro.local>
2023-12-17 16:58:24 +0900

    add file edit&delete

--- twitter-clone/src/components/tweet.tsx
+++ twitter-clone/src/components/tweet.tsx
@@ -1,9 +1,16 @@
 import { styled } from 'styled-components';
 import { ITweet } from './timeline';
 import { auth, db, storage } from '../firebase';
-import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
-import { deleteObject, ref } from 'firebase/storage';
+import { deleteDoc, deleteField, doc, updateDoc } from 'firebase/firestore';
+import {
+  deleteObject,
+  ref,
+  getDownloadURL,
+  uploadBytes,
+} from 'firebase/storage';
 import { useState } from 'react';
+import editIcon from '/edit.svg';
+import deleteIcon from '/remove.svg';
 
 const Wrapper = styled.div`
   display: grid;
@@ -18,6 +25,15 @@ const Wrapper = styled.div`
 `;
 
 const Column = styled.div``;
+
+const ColumnFile = styled.div`
+  position: relative;
+  &:hover {
+    & > div {
+      display: block;
+    }
+  }
+`;
 
 const Photo = styled.img`
   width: 100px;
@@ -88,17 +104,60 @@ const TextArea = styled.textarea`
   }
 `;
 
+const EditFileArea = styled.div`
+  display: none;
+`;
+
+const EditFile = styled.div`
+  position: absolute;
+  top: -14px;
+  right: 25px;
+  input {
+    display: none;
+  }
+  label {
+    display: inline-block;
+    width: 25px;
+    height: 25px;
+    font-size: 0;
+    background: rgba(0, 0, 0, 0.8) url(${editIcon}) no-repeat center;
+    background-size: 18px 18px;
+    cursor: pointer;
+  }
+`;
+
+const DeleteFile = styled.div`
+  position: absolute;
+  top: 0px;
+  right: 0;
+  width: 25px;
+  height: 25px;
+  font-size: 0;
+  background: rgba(255, 51, 28, 0.8) url(${deleteIcon}) no-repeat;
+  background-size: cover;
+  cursor: pointer;
+  span {
+    display: none;
+  }
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const [edit, setEdit] = useState(false);
   const [editedTweet, setEditedTweet] = useState('');
+  const [file, setFile] = useState<File | null>(null);
+  const [disable, setDisable] = useState(false);
   const user = auth.currentUser;
+  const photoRef = ref(storage, `tweets/${user?.uid}/${id}`);
+
+  const maxFileSizeInMB = 1;
+  const maxFileSizeInKB = 1024 * 1024 * maxFileSizeInMB;
+
   const onDelete = async () => {
     const ok = confirm('Are you sure you want to delete this tweet?');
     if (!ok || user?.uid !== userId) return;
     try {
       await deleteDoc(doc(db, 'tweets', id));
       if (photo) {
-        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
         await deleteObject(photoRef);
       }
     } catch (e) {
@@ -119,6 +178,45 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
     }
     setEdit(false);
   };
+  const onEditFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    //max file size is 1MB
+    if (files && files.length === 1 && files[0].size < maxFileSizeInKB) {
+      setFile(files[0]);
+      try {
+        if (user && photo) {
+          const result = await uploadBytes(photoRef, files[0]);
+          const url = await getDownloadURL(result.ref);
+          await updateDoc(doc(db, 'tweets', id), {
+            photo: url,
+          });
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (files && files[0].size > maxFileSizeInKB) {
+      alert(`Please select a file that is ${maxFileSizeInMB}MB or less.`);
+      setDisable(true);
+    } else {
+      alert('File uploaded successfully!');
+      setDisable(false);
+    }
+  };
+  const onDeleteFile = async () => {
+    if (user?.uid !== userId) return;
+    try {
+      if (photo) {
+        await deleteObject(photoRef);
+        await updateDoc(doc(db, 'tweets', id), {
+          photo: deleteField(),
+        });
+        location.reload();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <Wrapper>
       <Column>
@@ -132,7 +230,7 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
           <>
             <DeleteButton onClick={onDelete}>Delete</DeleteButton>
             {edit ? (
-              <EditButton onClick={onEdit} className="done">
+              <EditButton onClick={onEdit} className="done" disabled={disable}>
                 Done
               </EditButton>
             ) : (
@@ -142,9 +240,23 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
         ) : null}
       </Column>
       {photo ? (
-        <Column>
+        <ColumnFile>
           <Photo src={photo} />
-        </Column>
+          <EditFileArea>
+            <EditFile>
+              <label htmlFor="tweetFile">edit</label>
+              <input
+                type="file"
+                id="tweetFile"
+                accept="image/*"
+                onChange={onEditFile}
+              />
+            </EditFile>
+            <DeleteFile onClick={onDeleteFile}>
+              <span>delete</span>
+            </DeleteFile>
+          </EditFileArea>
+        </ColumnFile>
       ) : null}
     </Wrapper>
   );

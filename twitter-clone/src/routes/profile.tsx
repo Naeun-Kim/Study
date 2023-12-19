6596ee9c17fd457d3e9a7611c2392bd5d8635509
Naeun-Kim <naeun@Naeunui-MacBookPro.local>
2023-12-19 14:18:01 +0900

    create user profile avatar

--- twitter-clone/src/routes/profile.tsx
+++ twitter-clone/src/routes/profile.tsx
@@ -1,5 +1,70 @@
-import React from 'react';
+import { styled } from 'styled-components';
+import { auth, storage } from '../firebase';
+import { useState } from 'react';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
+import userIcon from '/user.svg';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+const Name = styled.span`
+  font-size: 22px;
+`;
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL); //interesting
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user?.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? <AvatarImg src={avatar} /> : <img src={userIcon} />}
+      </AvatarUpload>
+      <AvatarInput
+        id="avatar"
+        type="file"
+        accept="image/*"
+        onChange={onAvatarChange}
+      />
+      <Name>{user?.displayName ?? 'Anonymous'}</Name>
+    </Wrapper>
+  );
 }

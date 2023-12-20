5f054e5053cdb1550f3e03d97e260ad7c01812e1
Naeun-Kim <naeun@Naeunui-MacBookPro.local>
2023-12-20 09:49:41 +0900

    add profile name edit

--- twitter-clone/src/routes/profile.tsx
+++ twitter-clone/src/routes/profile.tsx
@@ -1,9 +1,20 @@
 import { styled } from 'styled-components';
-import { auth, storage } from '../firebase';
-import { useState } from 'react';
+import { auth, db, storage } from '../firebase';
+import { useEffect, useState, useRef } from 'react';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { updateProfile } from 'firebase/auth';
 import userIcon from '/user.svg';
+import { ITweet } from '../components/timeline';
+import Tweet from '../components/tweet';
+
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
 
 const Wrapper = styled.div`
   display: flex;
@@ -25,9 +36,10 @@ const AvatarUpload = styled.label`
     width: 50px;
   }
 `;
-
 const AvatarImg = styled.img`
   width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 const AvatarInput = styled.input`
   display: none;
@@ -35,10 +47,16 @@ const AvatarInput = styled.input`
 const Name = styled.span`
   font-size: 22px;
 `;
+const InputName = styled.input``;
+const Tweets = styled.div`
+  width: 100%;
+`;
 
 export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL); //interesting
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [editName, setEditName] = useState(false);
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (!user) return;
@@ -53,6 +71,61 @@ export default function Profile() {
       });
     }
   };
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, 'tweets'),
+      where('userId', '==', user?.uid),
+      orderBy('createdAt', 'desc'),
+      limit(25)
+    );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  const useOutsideClick = (callback: () => void) => {
+    const ref = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+          callback();
+        }
+      };
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [callback]);
+
+    return ref;
+  };
+  const nameInputRef = useOutsideClick(() => {
+    setEditName(false);
+  });
+  const onNameChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!user) return;
+    const userName = e.target.value;
+    await updateProfile(user, {
+      displayName: userName,
+    });
+  };
+  const handleNameEdit = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    console.log(e);
+    setEditName(!editName);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
@@ -64,7 +137,21 @@ export default function Profile() {
         accept="image/*"
         onChange={onAvatarChange}
       />
-      <Name>{user?.displayName ?? 'Anonymous'}</Name>
+      {editName ? (
+        <InputName
+          onChange={onNameChange}
+          type="text"
+          ref={nameInputRef}
+          autoFocus
+        />
+      ) : (
+        <Name onClick={handleNameEdit}>{user?.displayName ?? 'Anonymous'}</Name>
+      )}
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
     </Wrapper>
   );
 }
